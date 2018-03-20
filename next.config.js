6d0f9bbcecd1d6d05e368afc7ea22989dddd93f3
Robin Millette <robin@millette.info>
2018-03-19 21:29:56 -0400

    Export script.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 // next.config.js
 const withSass = require('@zeit/next-sass')
 module.exports = withSass({
+  exportPathMap: () => ({ '/': { page: '/' } }),
   sassLoaderOptions: {
     includePaths: [
       'node_modules/bulmaswatch',
