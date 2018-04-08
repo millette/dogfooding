67e944b5195fae915d14a0e00e7b23c6db6f1111
Robin Millette <robin@millette.info>
2018-04-08 15:46:15 -0400

    Add footer.

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import '../styles.scss'
 import NameForm from '../components/name-form'
 
 let author
-if (typeof window === 'object') { author = new URLSearchParams(window.location.search).get('author') }
+if (typeof window === 'object') { author = new window.URLSearchParams(window.location.search).get('author') }
 
 export default () => <div>
   <section className='hero is-primary'>
@@ -13,4 +13,24 @@ export default () => <div>
     </div>
   </section>
   <NameForm author={author} />
+  <footer className='footer'>
+    <div className='container'>
+      <p>&copy; 2018 <a href='http://robin.millette.info'>Robin Millette</a> AGPL-3.0</p>
+      <p>Sources sur github:</p>
+      <dl>
+        <dt>dogfooding (web front-end)</dt>
+        <dd>
+          <a href='https://github.com/millette/dogfooding'>
+            https://github.com/millette/dogfooding
+          </a>
+        </dd>
+        <dt>corsico (proxy cors)</dt>
+        <dd>
+          <a href='https://github.com/millette/corsico'>
+            https://github.com/millette/corsico
+          </a>
+        </dd>
+      </dl>
+    </div>
+  </footer>
 </div>
